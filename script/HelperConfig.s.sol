4d4746e43c9c424343b0aa8b3851321c214af954
Dan Magro <dando385@yahoo.com>
2025-07-12 18:40:57 -0400

    added helpercofig.s.sol

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,7 +3,31 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 
-contract HelperConfig is Script {
-    
+contract HelperConfig {
 
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 31337) {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        // Sepolia ETH/USD Address
+
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+
+        return sepoliaConfig;
+    }
+    
+    function getAnvilEthConfig() public pure {
+
+    }
 }
